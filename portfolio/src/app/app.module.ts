9d865901a53843f07383e81b35922f2eed35d866
sheikhimtiaz <imticorei54@gmail.com>
2021-01-12 15:03:53 +0600

    naming

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+import { AppComponent } from './components/app/app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
